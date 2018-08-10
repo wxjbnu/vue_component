0987c7cceb9eb89ea29c4444f9ff3a17ae94d6f0
wxj <139439432@qq.com>
2018-08-10 23:53:58 +0800

    0810 com

---
+++ templates/src/component.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "{{componentNamePascal}}", // vue component name
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+	<div class="{{componentName}}">
+	</div>
+</template>
+
+<style scoped>
+p{
+	margin: 0;
+}
+.{{componentName}} {
+	display: block;
+	width: 400px;
+	margin: 25px auto;
+	border: 1px solid #ccc;
+	background: #eaeaea;
+	text-align: center;
+	padding: 25px;
+}
+</style>
